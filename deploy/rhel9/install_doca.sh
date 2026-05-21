d18ab84cd5dc2c5c5b4b6adb028ece275f5654a0
kubestellar-hive <hive-bot@kubestellar.io>
2026-05-20 22:36:38 -0400

    fix(security): use HTTPS and enable gpgcheck in install_doca.sh
    Signed-off-by: kubestellar-hive <hive-bot@kubestellar.io>

--- deploy/rhel9/install_doca.sh
+++ deploy/rhel9/install_doca.sh
@@ -2,14 +2,16 @@
 #
 # https://docs.nvidia.com/dgx/dgx-el9-user-guide/installing-dofed-steps.html
 #
-sudo wget http://www.mellanox.com/downloads/ofed/RPM-GPG-KEY-Mellanox-SHA256
+sudo wget https://www.mellanox.com/downloads/ofed/RPM-GPG-KEY-Mellanox-SHA256
 sudo rpm --import RPM-GPG-KEY-Mellanox-SHA256
 sudo rpm -q gpg-pubkey --qf '%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n' | grep Mellanox
-sudo echo "[doca]
+sudo tee /tmp/doca.repo > /dev/null << 'EOF'
+[doca]
 name=DOCA Online Repo
 baseurl=https://linux.mellanox.com/public/repo/doca/DGX_latest_DOCA/rhel9/x86_64/
 enabled=1
-gpgcheck=0" > /tmp/doca.repo
+gpgcheck=1
+EOF
 sudo mv /tmp/doca.repo /etc/yum.repos.d/doca.repo
 sudo chown root.root /etc/yum.repos.d/doca.repo
 sudo dnf clean all -y
